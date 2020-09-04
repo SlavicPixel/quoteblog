3c073e943397092e9c4813d2a136cc4896c6ca74
SlavicPixel <35775576+SlavicPixel@users.noreply.github.com>
2020-09-04 20:52:37 +0200

    Added User Authentication

--- quoteblog/__init__.py
+++ quoteblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a14ca7f30abdb6b81e94e8866d205738'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from quoteblog import routes
